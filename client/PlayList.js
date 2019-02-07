f8938cb32fb4ac4cd98e67498deef054678344fe
dnErf <frend.fernando@live.com>
2019-02-07 15:35:58 -0800

    frontend frame init

---
+++ client/PlayList.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+import List from './components/List'
+import Info from './components/Info'
+
+const PlayList = () => {
+  return (
+    <div>
+      <List />
+      <Info />
+    </div>
+  );
+}
+
+export default PlayList;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './client/Playlist_Service.js',
+  entry: './client/',
   module: {
     rule: [
       {
